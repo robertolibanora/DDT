3cae103ee09dc35330fba019f01380b04c67c435
Roberto Libanora <roberto.libanora@gmail.com>
2026-01-13 17:59:12 +0100

    pdfcentro

--- app/static/js/layout_trainer.js
+++ app/static/js/layout_trainer.js
@@ -146,9 +146,13 @@ class LayoutTrainer {
     
     getMousePos(e) {
         const rect = this.canvas.getBoundingClientRect();
+        // Calcola il rapporto tra dimensioni CSS e dimensioni reali del canvas
+        const scaleX = this.canvas.width / rect.width;
+        const scaleY = this.canvas.height / rect.height;
+        
         return {
-            x: e.clientX - rect.left,
-            y: e.clientY - rect.top
+            x: (e.clientX - rect.left) * scaleX,
+            y: (e.clientY - rect.top) * scaleY
         };
     }
     
